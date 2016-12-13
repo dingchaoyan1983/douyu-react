6e823df29b4537ca00edbacfbe67b1970400267e
DingChao <270422767@qq.com>
2016-12-13 23:14:51 +0800

    use local images

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(props) {
     return (<div id="header">
                 <div className="head w1366head">
-                    <a className="head-logo fl" href="https://www.douyu.com/" style={{background: 'url("https://staticlive.douyucdn.cn/upload/signs/201610291926483131.png") left center no-repeat'}}></a>
+                    <a className="head-logo fl" href="https://www.douyu.com/"></a>
                     <ul className="head-nav fl">
                         <li className="fl index">
                             <a href="https://www.douyu.com/">首页</a>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Header from 'src/components/header';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'css/base.css';
-import 'css/scss/index.scss';
+import 'assets/css/base.css';
+import 'assets/css/scss/index.scss';
 
 const rootId = 'container';
 
