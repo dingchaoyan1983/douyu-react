043d8ef0019351f586062760d2171a8f5856842f
DingChao <270422767@qq.com>
2016-12-23 10:35:31 +0800

    add board component

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,6 +3,67 @@ import Channel from './channel';
 import SelectStateManager from 'src/components/common/select-state-manager';
 import SidebarTabs from './sidebar-tabs';
 import SidebarTab from './sidebar-tab';
+import Board from './board';
+
+const columnBoards = [
+    {
+        header: {
+            title: '热门游戏',
+            href: '/directory/columnRoom/game'
+        },
+        items: [
+            {"href":"https://www.douyu.com/directory/game/LOL","text":"英雄联盟"},
+            {"href":"https://www.douyu.com/directory/game/How","text":"炉石传说"},
+            {"href":"https://www.douyu.com/directory/game/DOTA2","text":"DOTA2"},
+            {"href":"https://www.douyu.com/directory/game/CF","text":"穿越火线"},
+            {"href":"https://www.douyu.com/directory/game/WOW","text":"魔兽世界"},
+            {"href":"https://www.douyu.com/directory/game/Overwatch","text":"守望先锋"},
+            {"href":"https://www.douyu.com/directory/game/SC","text":"星际争霸"},
+            {"href":"https://www.douyu.com/directory/game/FTG","text":"格斗游戏"},
+            {"href":"https://www.douyu.com/directory/game/TVgame","text":"主机游戏"},
+            {"href":"https://www.douyu.com/directory/game/DNF","text":"DNF"},
+            {"href":"https://www.douyu.com/directory/game/qipai","text":"棋牌娱乐"},
+            {"href":"https://www.douyu.com/directory/game/CSGO","text":"CS:GO"},
+            {"href":"https://www.douyu.com/directory/game/mszb","text":"魔兽争霸"},
+            {"href":"https://www.douyu.com/directory/game/classic","text":"怀旧游戏"}
+        ]
+    },
+    {
+        header: {
+            title: '移动游戏',
+            href: 'https://www.douyu.com/directory/columnRoom/ydyx'
+        },
+        items: [
+            {"href":"https://www.douyu.com/directory/game/wzry","text":"王者荣耀"},
+            {"href":"https://www.douyu.com/directory/game/yys","text":"阴阳师"},
+            {"href":"https://www.douyu.com/directory/game/qqdzz","text":"球球大作战"},
+            {"href":"https://www.douyu.com/directory/game/xyzx","text":"新游中心"},
+            {"href":"https://www.douyu.com/directory/game/hyrz","text":"火影忍者"},
+            {"href":"https://www.douyu.com/directory/game/hszz","text":"皇室战争"},
+            {"href":"https://www.douyu.com/directory/game/phone","text":"综合手游"},
+            {"href":"https://www.douyu.com/directory/game/mhzx","text":"梦幻诛仙"}
+        ]
+    }
+];
+
+const discoveryBoards = [
+    {
+        header: {
+            title: '我最喜欢'
+        },
+        items: [
+            {"href":"https://www.douyu.com/directory/game/mszb?is_hot=1","text":"魔兽争霸"},
+            {"href":"https://www.douyu.com/directory/game/xx?is_hot=1","text":"星秀"},
+            {"href":"https://www.douyu.com/directory/game/gm?is_hot=1","text":"国漫"},
+            {"href":"https://www.douyu.com/directory/game/LOL?is_hot=1","text":"英雄联盟"},
+            {"href":"https://www.douyu.com/directory/game/yz?is_hot=1","text":"颜值"},
+            {"href":"https://www.douyu.com/directory/game/outdoor?is_hot=1","text":"户外"},
+            {"href":"https://www.douyu.com/directory/game/Overwatch?is_hot=1","text":"守望先锋"},
+            {"href":"https://www.douyu.com/directory/game/smkj?is_hot=1","text":"数码科技"},
+            {"href":"https://www.douyu.com/directory/game/hszz?is_hot=1","text":"皇室战争"}
+        ]
+    }
+];
 
 export default function Sidebar(props) {
     return <div id="left" className="left-menu">
@@ -21,32 +82,14 @@ export default function Sidebar(props) {
                     </div>
                     <SidebarTabs initAcitveTabIndex={1}>
                         <SidebarTab name="栏目" className="icon-column">
-                            <dl>
-                                <dt className="cur">
-                                    <a href="/directory/columnRoom/game">
-                                        <i></i><span>热门游戏</span>
-                                    </a>
-                                </dt>
-                            </dl>
-                            <dd>
-                                <ul className="clearfix">
-                                     <li><a href="/directory/game/yx" title="鱼秀">鱼秀</a></li>
-                                </ul>
-                            </dd>
+                            {
+                                columnBoards.map(({header, items}) => <Board header={header} items={items}/>)
+                            }
                         </SidebarTab>
                         <SidebarTab name="发现" className="icon-recom">
-                            <dl>
-                                <dt className="cur">
-                                    <a href="/directory/columnRoom/game">
-                                        <i></i><span>热门游戏</span>
-                                    </a>
-                                </dt>
-                            </dl>
-                            <dd>
-                                <ul className="clearfix">
-                                     <li><a href="/directory/game/yx" title="鱼秀">鱼秀</a></li>
-                                </ul>
-                            </dd>
+                            {
+                                discoveryBoards.map(({header, items}) => <Board header={header} items={items}/>)
+                            }
                         </SidebarTab>
                     </SidebarTabs>
                 </div>
